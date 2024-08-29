8530bbe45821a5604be68901a99452869dcffc30
Hiten Gehlot <137190529+Iamhiten09@users.noreply.github.com>
2024-08-30 00:11:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import musicLibrary
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+    
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+        
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+        
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://linkedin.com")
+        
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = musicLibrary.music[song]
+        webbrowser.open(link)
+
+if __name__ == "__main__":
+    speak("Initializing Krishna...")
+    while True :
+        r = sr.Recognizer()
+     
+        print("recogizing...")
+        try :
+            with sr.Microphone() as source:
+            
+                print("Listening .....")
+                audio = r.listen(source, timeout=2 , phrase_time_limit=1)
+            
+            word = r.recognize_google(audio)
+            if(word.lower() == "krishna"):
+                speak("Ya")
+                with sr.Microphone() as source:
+            
+                    print("Krishna Active .....")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+                    
+                    processCommand(command)
+                
+            
+        except Exception as e:
+            print("Error; {0}".format(e))
---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "mashup": "https://youtu.be/NIT6jiX-9nk?si=W0y_x7rj2diyllYJ",
+    "bekhayali" : "https://youtu.be/yS-PD0zfnTc?si=9xQn1r4NvbsOKc-q",
+    "raanjhana" : "https://youtu.be/Kx-3u_Rz-3s?si=vWb31Olf1tMSBUek",
+    "lost sky" : "https://youtu.be/S19UcWdOA-I?si=f8mGpFjYWtXZRPzo"
+    
+}
